eaed9f16e897c0167cf64f4b35e87b522a646038
JS016354 <github@kreatious.net>
2015-03-03 12:48:49 -0600

    Adjusted draw style of on screen pedals to better emphasize user's input

--- src/main/java/net/kreatious/pianoleopard/painter/PlayedPedalStyleStrategy.java
+++ src/main/java/net/kreatious/pianoleopard/painter/PlayedPedalStyleStrategy.java
@@ -16,30 +16,24 @@ import net.kreatious.pianoleopard.midi.event.PedalEvent;
  */
 class PlayedPedalStyleStrategy implements StyleStrategy<PedalEvent> {
     private static final Stroke STROKE = new BasicStroke(2.0f);
+    private int smallestSeenChannel = 16;
 
     @Override
     public void paint(Graphics2D graphics, EventPair<PedalEvent> event, Rectangle rect) {
-        graphics.setColor(transparent(getPedalColor(event)));
+        // Certain keyboards send pedal events to multiple channels
+        if (event.getChannel() < smallestSeenChannel) {
+            smallestSeenChannel = event.getChannel();
+            return;
+        } else if (event.getChannel() != smallestSeenChannel) {
+            return;
+        }
+
+        graphics.setColor(transparent(ActivePedalStyleStrategy.getPedalColor(event)));
         graphics.setStroke(STROKE);
         graphics.fillRect(rect.x, rect.y, rect.width, rect.height);
 
-        graphics.setColor(getPedalColor(event));
+        graphics.setColor(ActivePedalStyleStrategy.getPedalColor(event));
         graphics.drawRect(rect.x, rect.y, rect.width, rect.height);
-    }
-
-    private static Color getPedalColor(EventPair<PedalEvent> pedal) {
-        switch (pedal.getOff().getPedal()) {
-        case PORTAMENTO:
-            return Color.BLUE.darker();
-        case SOFT:
-            return Color.DARK_GRAY.darker();
-        case SOSTENUTO:
-            return Color.RED.darker();
-        case SUSTAIN:
-            return Color.GREEN.darker();
-        default:
-            return Color.YELLOW.darker();
-        }
     }
 
     private static Color transparent(Color color) {
